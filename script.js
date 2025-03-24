ec80dda2c9707c1a10f4041a6c336583da5c9e56
Muhammad Fajri <160698852+Fajri135@users.noreply.github.com>
2025-03-24 22:11:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+//Date() untuk menampilkan tanggal saat ini
+Date()
+
+//Mendapatkan tanggal hari ini
+let tanggal = new Date();
+document.getElementById("tanggal").innerHTML = tanggal;
+
+//Untuk menampilkan alert tampil()
+function tampil() {
+    alert("Apa sih yang kamu cari?");
+}
+
+//Untuk Menampilkan alert show()
+function show() {
+    alert("Masih Belum Ketemu?");
+}
